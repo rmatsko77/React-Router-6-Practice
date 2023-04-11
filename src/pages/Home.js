133e3f0bdb41d2f5a4dba927fe6e422678c5ad10
Robert Matsko <robertmatsko@Roberts-MacBook-Pro-3.local>
2023-04-11 14:38:02 -0400

    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import {Link, Outlet} from 'react-router-dom';
+import Navbar from '../components/navbar';
+
+const Home = () => {
+  return (
+  <>
+   <h2>Home Page</h2>
+  </>
+  );
+};
+export default Home;
