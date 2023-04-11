49bb9570ad5f137f32f82e6c7b832ea3d2ae4463
Robert Matsko <robertmatsko@Roberts-MacBook-Pro-3.local>
2023-04-11 16:15:11 -0400

    final commit

--- src/components/StyledNavbar.js
+++ src/components/StyledNavbar.js
@@ -1,13 +1,31 @@
 import { NavLink } from "react-router-dom";
 
-const Navbar = () => {
+const StyledNavbar = () => {
     return (
         <nav className='navbar'>
-            <NavLink to='/'>Home</NavLink>
-            <NavLink to='/about'>About</NavLink>
-            <NavLink to='/products'>Products</NavLink>
+            <NavLink 
+            to='/'
+            className={({isActive}) => (isActive ? 'link active' : 'link')}
+            >
+                Home
+                </NavLink>
+            <NavLink to='/about'
+            className={({isActive}) => (isActive ? 'link active' : 'link')}
+            >
+                About
+                </NavLink>
+            <NavLink to='/products'
+            className={({isActive}) => (isActive ? 'link active' : 'link')}
+            >
+                Products
+                </NavLink>
+                <NavLink to='/login'
+            className={({isActive}) => (isActive ? 'link active' : 'link')}
+            >
+                Login
+                </NavLink>
         </nav>
     )
 }
 
-export default Navbar;
+export default StyledNavbar;
--- src/pages/SingleProduct.js
+++ src/pages/SingleProduct.js
@@ -1,7 +1,17 @@
+import {Link, useParams} from 'react-router-dom';
+import products from '../data';
+
+
 const SingleProduct = () => {
+  const {productId} = useParams();
+  const product = products.find((product) => product.id === productId);
+  const {image, name} = product;
+
   return (
     <section className='section product'>
-      <h2>single product</h2>
+      <h5>{name}</h5>
+      <img src={image} />
+      <Link to="/products">Back to Products</Link>
     </section>
   );
 };
